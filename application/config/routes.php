dd77d238e0e2bed4e4eccd1bc7ef504f1c993ac5
sally86 <si_86@hotmail.com>
2015-08-26 23:10:11 +0300

    routes fix bug

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['(:any)/(:any)'] = 'pages/view/$1/$1';
+$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = TRUE;
