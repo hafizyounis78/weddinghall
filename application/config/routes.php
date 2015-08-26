208d0571f2280924c8eb0388d731fbffa760cdda
sally86 <si_86@hotmail.com>
2015-08-26 23:06:05 +0300

    update route file in order to accept page/argument

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['(:any)'] = 'pages/view/$1';
+$route['(:any)/(:any)'] = 'pages/view/$1/$1';
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
