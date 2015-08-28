68604ae794a544450d9640b64be8e37e354514a7
sally86 <si_86@hotmail.com>
2015-08-29 01:04:33 +0300

    Add Payment JS

--- js/payments.js
+++ js/payments.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-	$('#btnAddpayments').click(function() {
-									
+	$('#btnAddpayments').click(function(event) {
+		event.preventDefault();
+		
 		$.ajax({
-			url: "pages/addpayments",
+			url: "http://localhost/weddinghall/pages/addpayments",
 			type: "POST",
 			data:  $("#payments_form").serialize(),
-			error: function(){
-				alert('error');
+			error: function(xhr, status, error) {
+  				//var err = eval("(" + xhr.responseText + ")");
+  				alert(xhr.responseText);
 			},
 			beforeSend: function(){},
 			complete: function(){},
 			success: function(){
 					alert ('تمت عملية الإضافة بنجاح');
+					window.location.href="http://localhost/weddinghall/payments";
 			}
 		});//END $.ajax
 	}); // END CLICK
