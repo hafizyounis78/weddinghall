9ff483b2cd2c09921c7df18c3e222f9eb81c3d3d
hafizyounis78 <hafizyounis@gmail.com>
2015-08-29 01:01:30 +0200

    addemppayemnts

---
+++ application/views/pages/addemppayments.php
@@ -0,0 +1,139 @@
+<?php
+
+
+if (isset($addemppayments))
+{
+foreach($addemppayments as $row);
+
+}
+
+?>
+
+<div class="row">
+				<div class="col-md-12">
+					<!-- BEGIN VALIDATION STATES-->
+              
+					<div class="portlet box green">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-gift"></i>Advance Validation
+							</div>
+							<div class="tools">
+								<a href="javascript:;" class="collapse">
+								</a>
+								<a href="#portlet-config" data-toggle="modal" class="config">
+								</a>
+								<a href="javascript:;" class="reload">
+								</a>
+								<a href="javascript:;" class="remove">
+								</a>
+							</div>
+						</div>
+						<div class="portlet-body form">
+							<!-- BEGIN FORM-->
+							<form action="#" id="emppayments_form" class="form-horizontal">
+								<div class="form-body">
+									<h3 class="form-section">Advance validation. <small>Custom radio buttons, checkboxes and Select2 dropdowns</small></h3>
+									
+                                    
+                                    <div class="alert alert-danger display-hide">
+										<button class="close" data-close="alert"></button>
+										You have some form errors. Please check below.
+									</div>
+									<div class="alert alert-success display-hide">
+										<button class="close" data-close="alert"></button>
+										Your form validation is successful!
+									</div>
+                                    <div class="form-group">
+										<label class="control-label col-md-3">كود الحجز <span class="required">
+										* </span>
+										</label>
+										<div class="col-md-4">
+											<input type="text" name="booking_code" <?php if (isset($row->booking_code)) {echo 'value="'.$row->booking_code.'"';}?>  data-required="1" class="form-control"/>
+										</div>
+									</div>
+<div class="form-group">
+										<label class="control-label col-md-3">اسم الصالة <span class="required">
+										* </span>
+										</label>
+										<div class="col-md-4">
+											<input type="text" name="w_name" <?php if (isset($row->w_name)) {echo 'value="'.$row->w_name.'"';}?>  data-required="1" class="form-control"/>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="control-label col-md-3">تاريخ الحجز</label>
+										<div class="col-md-4">
+											<div class="input-group date date-picker" data-date-format="yyyy/mm/dd">
+												<input type="text" class="form-control" readonly name="booking_date" <?php if (isset($row->booking_date)) {echo 'value="'.$row->booking_date.'"';}?>/>
+												<span class="input-group-btn">
+												<button class="btn default" type="button"><i class="fa fa-calendar"></i></button>
+												</span>
+											</div>
+											<!-- /input-group -->
+											<span class="help-block">
+											select a date </span>
+										</div>
+									</div>
+                                    
+                                    <div class="form-group">
+										<label class="control-label col-md-3">رقم الهوية <span class="required">
+										* </span>
+										</label>
+										<div class="col-md-4">
+											<input type="text" name="cut_id" <?php if (isset($row->cut_id)) {echo 'value="'.$row->cut_id.'"';}?>  data-required="1" class="form-control"/>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label class="control-label col-md-3">الاسم<span class="required">
+										* </span>
+										</label>
+										<div class="col-md-4">
+											<input type="text" name="name" data-required="1"  <?php if (isset($row->name)) {echo 'value="'.$row->name.'"';}?> class="form-control"/>
+										</div>
+									</div>
+                              		<div class="form-group">
+										<label class="control-label col-md-3">تاريخ الدفعة</label>
+										<div class="col-md-4">
+											<div class="input-group date date-picker" data-date-format="yyyy/mm/dd">
+												<input type="text" class="form-control" readonly name="payment_date"/>
+												<span class="input-group-btn">
+												<button class="btn default" type="button"><i class="fa fa-calendar"></i></button>
+												</span>
+											</div>
+											<!-- /input-group -->
+											<span class="help-block">
+											select a date </span>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="control-label col-md-3">قيمة الدفعة</label>
+										<div class="col-md-4">
+											<input name="payment_amount" type="text" class="form-control"/>
+										</div>
+                                     </div>
+                                     <div class="form-group">
+										<label class="control-label col-md-3">رقم الوصل المالي</label>
+										<div class="col-md-4">
+											<input name="invoice_no" type="text" class="form-control"/>
+                                            
+										</div>
+                                     </div>
+                                     
+								<div class="form-actions">
+									<div class="row">
+										<div class="col-md-offset-3 col-md-9">
+											<button id="btnAddemppayments" name="btnAddemppayments" type="submit"  class="btn green">Submit</button>
+											<button type="button" class="btn default">Cancel</button>
+										</div>
+									</div>
+								</div>
+							</form>
+							<!-- END FORM-->
+						</div>
+						<!-- END VALIDATION STATES-->
+					</div>
+				</div>
+			</div>
+            
+								
