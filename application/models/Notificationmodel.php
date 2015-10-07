1da411ad31a76b791e9593b5f6dbaffee08001b3
hafizyounis78 <hafizyounis@gmail.com>
2015-10-08 01:06:10 +0200

    update notificaton
    5

--- application/models/Notificationmodel.php
+++ application/models/Notificationmodel.php
@@ -8,7 +8,7 @@ public function get_booking_notification()
 		date_default_timezone_set('Asia/Gaza');   
 		$today_date = date('Y-m-d');
 		//$today_date= $dt->format("yyyy-mm-dd");
-		$after_date=date('Y-m-d', strtotime('+3 days'));
+		$after_date=date('Y-m-d', strtotime('+5 days'));
 		$myquery = "SELECT 	wedding_booking. * , customer. * , wedding_hall. * , booking_status_tb . *
 					FROM 	wedding_booking, customer, wedding_hall, booking_status_tb
 					WHERE 	wedding_booking.w_code = wedding_hall.w_code
@@ -24,7 +24,7 @@ public function get_booking_notification_count()
 		date_default_timezone_set('Asia/Gaza');   
 		$today_date = date('Y-m-d');
 		//$today_date= $dt->format("yyyy-mm-dd");
-		$after_date=date('Y-m-d', strtotime('+3 days'));
+		$after_date=date('Y-m-d', strtotime('+5 days'));
 		$myquery = "SELECT 	count(1) as cnt
 					FROM 	wedding_booking, customer, wedding_hall, booking_status_tb
 					WHERE 	wedding_booking.w_code = wedding_hall.w_code
